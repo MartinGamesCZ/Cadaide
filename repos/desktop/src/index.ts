9dab8cac3ac1d950f16be63c188c3d9892dc11fb
Martin Petr <martinpetrnp@gmail.com>
2026-03-30 19:37:30 +0200

    Add desktop wrapper

---
+++ repos/desktop/src/index.ts
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from "electron";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
