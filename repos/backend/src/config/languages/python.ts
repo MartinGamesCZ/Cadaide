b823a3698f039f234a13aedee6afb07534df43c9
Martin Petr <martinpetrnp@gmail.com>
2026-04-05 13:27:56 +0200

    Fix build runtime

--- repos/backend/src/config/languages/python.ts
+++ repos/backend/src/config/languages/python.ts
@@ -2,6 +2,14 @@ import { ILanguageConfig } from '../languages';
 
 export const PythonLanguageConfig: ILanguageConfig = {
   lsp: {
-    command: ['npx', '--yes', '-p', 'pyright', 'pyright-langserver', '--stdio'],
+    command: [
+      process.env.BUN_BINARY_PATH ?? 'bun',
+      'x',
+      '--yes',
+      '-p',
+      'pyright',
+      'pyright-langserver',
+      '--stdio',
+    ],
   },
 };
--- repos/backend/src/main.ts
+++ repos/backend/src/main.ts
@@ -8,6 +8,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors('*');
+
   app.useWebSocketAdapter(new WsAdapter(app));
   app.useGlobalPipes(new ValidationPipe());
 
